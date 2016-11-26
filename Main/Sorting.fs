fd24eca1a01498bc7a3689e4e61b0268d8de924d
120944 <lucashbp@gmail.com>
2016-11-26 16:11:56 +0100

    Added ex1 logic

---
+++ Main/Sorting.fs
@@ -0,0 +1,33 @@
+﻿module Sorting
+    open Microsoft.Xna.Framework
+    open System
+
+    let rec split_on = function 
+        | (n, []) -> ([],[])
+        | (1, x::xs) -> (x::[],xs)
+        | (n, x::xs) ->
+            let (l, r) = split_on (n-1, xs)
+            x::l, r
+
+    let split = fun (list : 'a list) -> 
+        split_on (list.Length/2, list)
+
+    let rec mergeList = fun (selector:Func<'a,'b>) l r ->
+        match (l, r) with
+        | [], l -> l
+        | l, [] -> l
+        | x::xs, y::ys ->
+            if (selector.Invoke(x)) <= (selector.Invoke(y)) then
+                x :: (mergeList selector xs (y::ys))
+            else
+                y :: (mergeList selector (x::xs) ys)
+
+    let rec mergeSort = fun (selector:Func<'a,'b>) (list : 'a list) ->
+        match list with
+        | [] -> [] // no elements or end of list
+        | [x] -> [x] // one element or last element
+        | xs ->
+            let (l, r) = split xs
+            let l_sorted = mergeSort selector l
+            let r_sorted = mergeSort selector r
+            mergeList selector l_sorted r_sorted
